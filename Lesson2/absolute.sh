1a936702616c6c62c64f0201e317a313ecbc75f0
helix <helix@debian-BULLSEYE-live-builder-AMD64>
2022-09-09 15:39:25 +0300

    Lesson2/absolute.sh

---
+++ Lesson2/absolute.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir /tmp/hello_world
+touch /tmp/hello_world/absolute.txt
+echo 'hello world!!' > /tmp/hello_world/absolute.txt
+cat /tmp/hello_world/absolute.txt
+ls -la /tmp/hello_world/absolute.txt
