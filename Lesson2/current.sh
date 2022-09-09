ada777e6754f07500e7878cb359da5cb45455650
helix <helix@debian-BULLSEYE-live-builder-AMD64>
2022-09-09 15:53:15 +0300

    Lesson2/current.sh

---
+++ Lesson2/current.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir ./hello_current
+touch ./hello_current/current.txt
+echo 'hello current!!' > ./hello_current/current.txt
+cat ./hello_current/current.txt
+ls -la ~/hello_current/current.txt
