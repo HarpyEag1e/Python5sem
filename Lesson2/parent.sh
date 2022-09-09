f4bd28d40bc5d2d2e0b329dfd9e89a872157ae68
helix <helix@debian-BULLSEYE-live-builder-AMD64>
2022-09-09 15:59:54 +0300

    Lesson2/current.sh Lesson2/parent.sh

---
+++ Lesson2/parent.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+mkdir ../hello_parent
+touch ../hello_parent/parent.txt
+echo 'hello parent!!' > ../hello_parent/parent.txt
+cat ../hello_parent/parent.txt
+ls -la ../hello_parent/parent.txt
